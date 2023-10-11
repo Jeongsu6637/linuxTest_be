5277f4fb4a16189b3434e887793036a50d612b74
Jeongsu6637 <wjdtn8738@gmail.com>
2023-10-11 14:03:32 +0900

    1234

--- src/test/java/com/example/gcptest/GcpTestApplicationTests.java
+++ src/test/java/com/example/gcptest/GcpTestApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.gcptest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GcpTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void test() {
+        Assertions.assertEquals(1+1, 2);
+    }
 
 }
